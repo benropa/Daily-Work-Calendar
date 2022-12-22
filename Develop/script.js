94a3e50a45ad2b9715de4a53e1e1529c7cb689e1
Ben Ropa <ben@Bropas-MBP.lan>
2022-12-22 15:56:02 -0600

    tweaking starter code and adding course materials

--- Develop/script.js
+++ Develop/script.js
@@ -21,3 +21,66 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+$(document).ready(function () {
+  // listen for save button clicks
+  $('.saveBtn').on('click', function () {
+
+    // get nearby values
+    var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    // save in localStorage
+    localStorage.setItem(time, value);
+
+    // Show notification that item was saved to localStorage by adding class 'show'
+    $('.notification').addClass('show');
+
+    // Timeout to remove 'show' class after 5 seconds
+    setTimeout(function () {
+      $('.notification').removeClass('show');
+    }, 5000);
+  });
+
+  function hourUpdater() {
+    // get current number of hours
+    var currentHour = dayjs().hour();
+
+    // loop over time blocks
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
+
+  hourUpdater();
+
+  // set up interval to check if current time needs to be updated
+  setInterval(hourUpdater, 15000);
+
+  // load any saved data from localStorage
+  $('#hour-8 .description').val(localStorage.getItem('hour-8'));
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+  // display current day on page
+  $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY h:mmA'));
+});
